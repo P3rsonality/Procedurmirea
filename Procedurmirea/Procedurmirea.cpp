94cbe447f5337c95195621f5f6de160d17f72a72
P3rsonal1ty <P3rsonal1ty@DESKTOP-9J2O191>
2023-10-04 21:40:20 +0300

    Добавьте файлы проекта.

---
+++ Procedurmirea/Procedurmirea.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+git init
+git add README.md
+git commit - m "first commit"
+git branch - M main
+git remote add origin https ://github.com/P3rsonality/procedurmirea.git
+git push - u origin main
+int main()
+{
+    std::cout << "Hello World!\n";
+}

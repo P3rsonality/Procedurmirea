1da550fb89e64346b86fb1d7807c58c414e57755
Шерстнёв Виталий <qwertwert82@mail.ru>
2023-10-06 00:35:53 +0300

    my changes

--- Procedurmirea/Procedurmirea.cpp
+++ Procedurmirea/Procedurmirea.cpp
@@ -217,9 +217,7 @@ void bk() {
 	setlocale(LC_ALL, "Russian");
 	srand(time(0));
 	int randomcheck[10]{ 0 };
-	int chislo, check, check1, checkchislo;
-	int start = 0, end = 9, r = 4, plus = 0;
-	int chislocomp = 0, minus = 0;
+	int chislo, check, check1, checkchislo, start = 0, end = 9, r = 4, plus = 0, chislocomp = 0, minus = 0;
 	for (int i = 0; i < r; i++) {
 		int random = rand() % (end - start + 1) + start;
 		if (i == 0 && random == 0) i--;
@@ -263,13 +261,105 @@ void bk() {
 		else cout << plus << " Плюса " << minus - plus << " Минуса" << endl;
 	}
 }
-void test() {
-    srand(time(0));
-    int start = 0, end = 9, r = 4, plus = 0;
-    int random = rand() % (end - start + 1) + start;
-    cout << random;
+void monach() {
+    setlocale(LC_ALL, "Russian");
+    int monachinfo[600][3]{ 0 };
+    int nummonach[600]{ 0 };
+    int uchitel, uchenik1, uchenik2, uchenik3, n, n1, checkmonah, checkmonah2, r = 0, r1 = 0,perm1,perm2;
+    cout << "Введите количество строк данных: ";
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cout << "Введите учителя и его учеников: ";
+        cin >> uchitel >> uchenik1 >> uchenik2 >> uchenik3;
+        monachinfo[uchitel][0] = uchenik1;
+        monachinfo[uchitel][1] = uchenik2;
+        monachinfo[uchitel][2] = uchenik3;
+        nummonach[uchitel] = 1;
+        nummonach[uchenik1] = 1;
+        nummonach[uchenik2] = 1;
+        nummonach[uchenik3] = 1;
+    }
+    cout << "Введите количество проверяемых монахов для части <1>:";
+    cin >> n1;
+    for (int k = 0; k < n1; k++) {
+        int uchmonaha[600]{ 0 };
+        r = 0;
+        cout << "Введите проверяемого монаха:";
+        cin >> checkmonah;
+        perm1 = checkmonah;
+        if (nummonach[checkmonah] == 1) {
+            cout << checkmonah << " - монах, его учителя ";
+            for (int i = perm1; i >= 0; i--) {
+                for (int j = 0; j < 3; j++) {
+                    if (monachinfo[i][j] == checkmonah && r < 2) {
+                        uchmonaha[checkmonah] = i;
+                        checkmonah = i;
+                        r++;
+                    }
+                }
+            }
+            for (int i = perm1; i >= 0; i--) {
+                if (uchmonaha[i] != 0) cout << uchmonaha[i]  << " ";
+            }
+            cout << endl;
+        }
+        else {
+            cout << checkmonah << " - не монах" << endl;
+        }
+    }
+    cout << "Введите количество проверяемых монахов для части <2>:";
+    cin >> n1;
+    for (int k = 0; k < n1; k++) {
+        int uchmonaha1[600]{ 0 };
+        int uchmonaha2[600]{ 0 };
+        r1 = 0;
+        cout << "Введите проверяемых монахов:";
+        cin >> checkmonah >> checkmonah2;
+        perm1 = checkmonah;
+        perm2 = checkmonah2;
+        if (nummonach[checkmonah] == 1 && nummonach[checkmonah2] == 1) {
+            cout << checkmonah << " и " << checkmonah2 << " - оба монахи, и их общий учитель ";
+            for (int i = perm1; i >= 0; i--) {
+                for (int j = 0; j < 3; j++) {
+                    if (monachinfo[i][j] == checkmonah) {
+                        uchmonaha1[checkmonah] = i;
+                        checkmonah = i;
+                    }
+                }
+            }
+            for (int i = perm2; i >= 0; i--) {
+                for (int j = 0; j < 3; j++) {
+                    if (monachinfo[i][j] == checkmonah2) {
+                        uchmonaha2[checkmonah2] = i;
+                        checkmonah2 = i;
+                    }
+                }
+            }
+            for (int i = max(perm1,perm2); i >= 0; i--) {
+                for (int j = max(perm1, perm2); j >= 0; j--) {
+                    if (uchmonaha1[i]==uchmonaha2[j] && uchmonaha1[i]!=0 && r1==0) {
+                        cout << uchmonaha1[i] << " ";
+                        r1++;
+                    }
+                }
+            }
+            cout << endl;
+        }
+        else {
+            if (nummonach[checkmonah] != 1 && nummonach[checkmonah2] != 1) cout << checkmonah << " и " << checkmonah2 << " - оба не монахи" << endl;
+            else {
+                if (nummonach[checkmonah2] == 1) {
+                    cout << checkmonah << " - не монах" << endl;
+                }
+                else cout << checkmonah2 << " - не монах" << endl;
+            }
+        }
+    }
 }
 int main()
 {
-	dz4_4();
+    //bk();
+    monach();
+	//dz4_4();
+
 }

c199cd2030d7dd9025a02c2bf30583cce39f2d77
Шерстнёв Виталий <qwertwert82@mail.ru>
2023-10-10 00:38:58 +0300

    my changes

--- Procedurmirea/Procedurmirea.cpp
+++ Procedurmirea/Procedurmirea.cpp
@@ -6,6 +6,8 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <windows.h>
+#include <string>
+#include <algorithm>
 using namespace std;
 void dz2_1() {
     setlocale(LC_ALL, "Russian");
@@ -261,105 +263,162 @@ void bk() {
 		else cout << plus << " Плюса " << minus - plus << " Минуса" << endl;
 	}
 }
-void monach() {
+void file2() {
     setlocale(LC_ALL, "Russian");
-    int monachinfo[600][3]{ 0 };
-    int nummonach[600]{ 0 };
-    int uchitel, uchenik1, uchenik2, uchenik3, n, n1, checkmonah, checkmonah2, r = 0, r1 = 0,perm1,perm2;
-    cout << "Введите количество строк данных: ";
-    cin >> n;
+    int n = 10;
+    int zoloto, bronza, cerebro, schet, vsego;
+    int topschet[10]{ 0 };
+    int topvsego[10]{ 0 };
+    int topzoloto[10]{ 0 };
+    int topbronza[10]{ 0 };
+    int topcerebro[10]{ 0 };
+    string topcountry[10], country;
+    ifstream Fin("file2.txt");
+    for (int i = 0; i < 10; i++) {
+        Fin >> country;
+        cout << country << " -> ";
+        cin >> zoloto >> cerebro >> bronza;
+        vsego = zoloto + cerebro + bronza;
+        schet = zoloto * 7 + cerebro * 6 + bronza * 5;
+        for (int k = 0; k < 10; k++) {
+            if (schet >= topschet[k]) {
+                for (int j = n - 2; j >= k; j--) {
+                    topschet[j + 1] = topschet[j];
+                    topvsego[j + 1] = topvsego[j];
+                    topzoloto[j + 1] = topzoloto[j];
+                    topcerebro[j + 1] = topcerebro[j];
+                    topbronza[j + 1] = topbronza[j];
+                    topcountry[j + 1] = topcountry[j];
+                }
+                topschet[k] = schet;
+                topvsego[k] = vsego;
+                topzoloto[k] = zoloto;
+                topcerebro[k] = cerebro;
+                topbronza[k] = bronza;
+                topcountry[k] = country;
+                k += 11;
+            }
+        }
+    }
+    ofstream File("top.txt");
+    File << "\t" << "Страна" << "\t" << "Золото" << "\t" << "Серебро" << "\t" << "Бронза" << "\t" << "Всего" << "\t" << "Очков" << endl;
+    File << endl;
     for (int i = 0; i < n; i++) {
-        cout << "Введите учителя и его учеников: ";
-        cin >> uchitel >> uchenik1 >> uchenik2 >> uchenik3;
-        monachinfo[uchitel][0] = uchenik1;
-        monachinfo[uchitel][1] = uchenik2;
-        monachinfo[uchitel][2] = uchenik3;
-        nummonach[uchitel] = 1;
-        nummonach[uchenik1] = 1;
-        nummonach[uchenik2] = 1;
-        nummonach[uchenik3] = 1;
+        File << i + 1 << "\t" << topcountry[i] << "\t" << topzoloto[i] << "\t" << topcerebro[i] << "\t" << topbronza[i] << "\t" << topvsego[i] << "\t" << topschet[i] << endl;
+
+    }
+
+}
+int poisk(int *n, int k) {
+    if (k != 1) { 
+        if (n[k] != 0) {
+            poisk(n, k = n[k]);
+            return k;
+        }
+        else return 0;
+    }
+}
+void optimizmonach() {
+    int uchmonaha[600]{ 0 };
+    int uchenik[3]{ 0 };
+    int uchitel, uchenik1, uchenik2, n, n1;
+    cout << "Введите количество строк данных: "; 
+    cin >> n;
+    for (int i = 0; i < n; i++) { 
+        cout << "Введите учителя и его учеников: "; 
+        cin >> uchitel; 
+        for (int i = 0; i < 3; i++) {
+            cin >> uchenik[i]; 
+            uchmonaha[uchenik[i]] = uchitel;
+        }
     }
     cout << "Введите количество проверяемых монахов для части <1>:";
     cin >> n1;
-    for (int k = 0; k < n1; k++) {
-        int uchmonaha[600]{ 0 };
-        r = 0;
-        cout << "Введите проверяемого монаха:";
-        cin >> checkmonah;
-        perm1 = checkmonah;
-        if (nummonach[checkmonah] == 1) {
-            cout << checkmonah << " - монах, его учителя ";
-            for (int i = perm1; i >= 0; i--) {
-                for (int j = 0; j < 3; j++) {
-                    if (monachinfo[i][j] == checkmonah && r < 2) {
-                        uchmonaha[checkmonah] = i;
-                        checkmonah = i;
-                        r++;
-                    }
-                }
+    for (int i = 0; i < n1; i++) {
+        cin >> uchenik1; 
+        int num=uchenik1; 
+        for (int j = 0; j < n; j++) { 
+            uchenik1 = poisk(uchmonaha, uchenik1);
+            if (uchenik1 >1) cout << uchenik1<<" ";
+            else {
+                if(uchenik1==0) cout <<num << " - не монах" << endl;
+                break;
             }
-            for (int i = perm1; i >= 0; i--) {
-                if (uchmonaha[i] != 0) cout << uchmonaha[i]  << " ";
-            }
-            cout << endl;
-        }
-        else {
-            cout << checkmonah << " - не монах" << endl;
         }
     }
     cout << "Введите количество проверяемых монахов для части <2>:";
     cin >> n1;
-    for (int k = 0; k < n1; k++) {
-        int uchmonaha1[600]{ 0 };
-        int uchmonaha2[600]{ 0 };
-        r1 = 0;
-        cout << "Введите проверяемых монахов:";
-        cin >> checkmonah >> checkmonah2;
-        perm1 = checkmonah;
-        perm2 = checkmonah2;
-        if (nummonach[checkmonah] == 1 && nummonach[checkmonah2] == 1) {
-            cout << checkmonah << " и " << checkmonah2 << " - оба монахи, и их общий учитель ";
-            for (int i = perm1; i >= 0; i--) {
-                for (int j = 0; j < 3; j++) {
-                    if (monachinfo[i][j] == checkmonah) {
-                        uchmonaha1[checkmonah] = i;
-                        checkmonah = i;
-                    }
-                }
-            }
-            for (int i = perm2; i >= 0; i--) {
-                for (int j = 0; j < 3; j++) {
-                    if (monachinfo[i][j] == checkmonah2) {
-                        uchmonaha2[checkmonah2] = i;
-                        checkmonah2 = i;
-                    }
-                }
-            }
-            for (int i = max(perm1,perm2); i >= 0; i--) {
-                for (int j = max(perm1, perm2); j >= 0; j--) {
-                    if (uchmonaha1[i]==uchmonaha2[j] && uchmonaha1[i]!=0 && r1==0) {
-                        cout << uchmonaha1[i] << " ";
-                        r1++;
-                    }
-                }
-            }
-            cout << endl;
+    for (int i = 0; i < n1; i++) { 
+        cin >> uchenik1 >> uchenik2;
+        if (uchmonaha[uchenik1] == 0 && uchmonaha[uchenik2] == 0) {
+            cout << " - оба не монахи";
+            break;
         }
-        else {
-            if (nummonach[checkmonah] != 1 && nummonach[checkmonah2] != 1) cout << checkmonah << " и " << checkmonah2 << " - оба не монахи" << endl;
+        if (uchmonaha[uchenik1] == 0) {
+            cout << uchenik1 << " -не монах";
+            break;
+        }
+        else if (uchmonaha[uchenik2] == 0) {
+            cout << uchenik2 << " - не монах";
+            break;
+        }
+        int cycle1 = max(uchenik1, uchenik2);
+        for (int i = 0; i < cycle1; i++) {
+            if (uchenik1 > uchenik2) uchenik1 = poisk(uchmonaha, uchenik1);
             else {
-                if (nummonach[checkmonah2] == 1) {
-                    cout << checkmonah << " - не монах" << endl;
+                if (uchenik1 < uchenik2) uchenik2 = poisk(uchmonaha, uchenik2);
+                if (uchenik1 == uchenik2) {
+                    cout << uchenik1;
+                    break;
                 }
-                else cout << checkmonah2 << " - не монах" << endl;
             }
         }
     }
 }
+bool perestanovka(int* kol, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        if (kol[i] == i)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+long double factorial(int N)
+{
+    if (N < 0) 
+        return 0;
+    if (N == 0) 
+        return 1;
+    else
+        return N * factorial(N - 1);
+}
+int shariki() {
+    int res = 0, n, factor;
+    cin >> n;
+    factor = factorial(n);
+    int* kol = new int[n] {0};
+    for (int i = 0; i < n; i++) {
+        kol[i] = i;
+    }
+    for (int i = 0; i < factor; i++)
+    {
+        next_permutation(kol, kol +n);
+        if (perestanovka(kol, n)==1)
+            res++;
+    }
+    cout << res << endl;
+
+    return 0;
+}
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     //bk();
-    monach();
+    optimizmonach();
+    //sinusoida();
 	//dz4_4();
-
+    //shariki();
 }
